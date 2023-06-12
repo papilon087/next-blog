4bc06563cc3198265b8d08dcfc720759de2b7bac
papilon <ademirgus@gmail.com>
2023-06-12 19:54:52 -0300

    init:configuração inicial

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,17 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
+import { Poppins } from "@next/font/google";
+import "@/styles/globals.css";
+
+const poppings = Poppins({
+  weight: ["400", "700"],
+  subsets: ["latin"],
+  variable: "--font-poppins",
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <main className={`${poppings.variable} font-sans`}>
+      <Component {...pageProps} />;
+    </main>
+  );
 }
