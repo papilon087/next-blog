e95abbea0812a7bd945e792eff93a701891b1920
papilon <ademirgus@gmail.com>
2023-06-16 16:58:12 -0300

    feat:creação do componente empty

---
+++ src/components/Empty.tsx
@@ -0,0 +1,7 @@
+export function Empty() {
+  return (
+    <div className="flex w-full h-screen items-center justify-center">
+      <h1 className="font-bold text-xl text-zinc-900">Ainda não existe POST publicado no momento...</h1>
+    </div>
+  )
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,6 +6,7 @@ import { gql, useQuery } from '@apollo/client'
 import { CardPost } from "@/components/CardPost";
 import { Header } from "@/components/Header";
 import { Loading } from "@/components/Loading";
+import { Empty } from "@/components/Empty";
 
 export default function Home() {
 
@@ -57,55 +58,61 @@ export default function Home() {
 
       <div className="w-full max-w-[1120px] flex flex-col mx-auto pb-12 px-4">
         <Header />
-
-        <Link
-          href={"/post"}
-          className="w-full h-full flex gap-4 lg:gap-8 flex-col sm:flex-row items-center justify-center mt-12 hover:brightness-75 transition-all"
-        >
-          <div className="flex flex-1 w-full h-full min-h-[240px] md:min-h-[334px] relative rounded-2xl overflow-hidden">
-            <Image
-              src={data?.posts[0].coverImage.url}
-              alt=""
-              fill={true}
-              style={{ objectFit: "cover" }}
-            />
-          </div>
-
-          <div className="flex flex-1 h-full flex-col gap-3 lg:gap-6">
-            <h1 className="font-bold text-3xl md:text-[40px] text-blue-600">
-              {data?.posts[0].title}
-            </h1>
-            <p className="text-zinc-600 text-sm md:text-base text-justify lg:text-left">
-            {data?.posts[0].subtile}
-            </p>
-
-            <div>
-              <p className="font-bold text-zinc-900 text-sm md:text-base">
-                {data?.posts[0].title}
-              </p>
-              <p className="text-zinc-600 text-xs md:text-sm">
-                {data?.posts[0].createdAt}
-              </p>
+      
+        {data ? 
+          <>
+            <Link
+            href={"/post"}
+            className="w-full h-full flex gap-4 lg:gap-8 flex-col sm:flex-row items-center justify-center mt-12 hover:brightness-75 transition-all"
+          >
+            <div className="flex flex-1 w-full h-full min-h-[240px] md:min-h-[334px] relative rounded-2xl overflow-hidden">
+              <Image
+                src={data?.posts[0].coverImage.url}
+                alt=""
+                fill={true}
+                style={{ objectFit: "cover" }}
+              />
             </div>
-          </div>
-        </Link>
 
-        <div className="flex flex-col items-center sm:grid grid-cols-2 md:grid-cols-3 gap-4 lg:gap-8 mt-12">
-          {data?.posts.map((post, index) => {
-            if (index !== 0) {
-              return (
-                <CardPost 
-                  key={post.id}
-                  title={post.title}
-                  author={post.author.name}
-                  createdAt={post.createdAt}
-                  subtile={post.subtile}
-                  urlImage={post.coverImage.url}
-                />  
-              )
-            }
-          })}
-        </div>
+            <div className="flex flex-1 h-full flex-col gap-3 lg:gap-6">
+              <h1 className="font-bold text-3xl md:text-[40px] text-blue-600">
+                {data?.posts[0].title}
+              </h1>
+              <p className="text-zinc-600 text-sm md:text-base text-justify lg:text-left">
+              {data?.posts[0].subtile}
+              </p>
+
+              <div>
+                <p className="font-bold text-zinc-900 text-sm md:text-base">
+                  {data?.posts[0].title}
+                </p>
+                <p className="text-zinc-600 text-xs md:text-sm">
+                  {data?.posts[0].createdAt}
+                </p>
+              </div>
+            </div>
+          </Link>
+
+          <div className="flex flex-col items-center sm:grid grid-cols-2 md:grid-cols-3 gap-4 lg:gap-8 mt-12">
+            {data?.posts.map((post, index) => {
+              if (index !== 0) {
+                return (
+                  <CardPost 
+                    key={post.id}
+                    title={post.title}
+                    author={post.author.name}
+                    createdAt={post.createdAt}
+                    subtile={post.subtile}
+                    urlImage={post.coverImage.url}
+                  />  
+                )
+              }
+            })}
+          </div>
+        </>
+        :
+        <Empty />
+       }
       </div>
     </>
   );
