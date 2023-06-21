9fcbb61523fd7e0cdffacf1903fab6c7f8e9a3e5
papilon <ademirgus@gmail.com>
2023-06-21 15:55:36 -0300

    feat:fallback, paths and revalidate

--- src/pages/post/[slug].tsx
+++ src/pages/post/[slug].tsx
@@ -1,9 +1,10 @@
 import Head from "next/head";
 import Image from "next/image";
 import Link from "next/link";
-import { gql } from "@apollo/client";
 import { GetStaticPaths, GetStaticProps } from "next";
+import { gql } from "@apollo/client";
 import { client } from "@/lib/apollo";
+import { format } from "date-fns";
 
 import { Header } from "@/components/Header";
 
@@ -43,7 +44,7 @@ interface PostProps {
   };
 }
 
-export default function Post() {
+export default function Post({ post }: PostProps) {
   return (
     <>
       <Head>
@@ -65,7 +66,7 @@ export default function Post() {
         <div className="w-full h-full flex flex-col mt-8">
           <div className="flex w-full h-56 sm:h-80 lg:h-[392px] relative rounded-2xl overflow-hidden">
             <Image
-              src="https://images.unsplash.com/photo-1555066931-4365d14bab8c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80"
+              src={post.coverImage.url}
               alt=""
               fill={true}
               style={{ objectFit: "cover" }}
@@ -74,11 +75,14 @@ export default function Post() {
 
           <div className="flex w-full flex-col mt-4 sm:mt-8">
             <h1 className="font-bold text-2xl sm:text-4xl lg:text-[40px] text-blue-600">
-              Como desenvolver um Blog com Next.js
+              {post.title}
             </h1>
             <div>
-              <p className="font-bold text-zinc-900">Ademir Gustavo</p>
-              <p className="text-zinc-600 text-sm">31 de Janeiro de 2023</p>
+              <p className="font-bold text-zinc-900">{post.author.name}</p>
+              <p className="text-zinc-600 text-sm">
+                {" "}
+                {format(new Date(post.createdAt), "dd 'de' MMM 'de' yyyy")}
+              </p>
             </div>
             <p className="text-zinc-600 text-sm sm:text-base text-justify lg:text-left mt-4 sm:mt-8">
               Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ac
@@ -92,6 +96,7 @@ export default function Post() {
   );
 }
 
+//Fazendo a chamada na API passando dentro das props, passando como post destribuindo nas páginas de forma static;
 export const getStaticProps: GetStaticProps = async (ctx) => {
   const slug = ctx.params?.slug;
 
@@ -101,16 +106,19 @@ export const getStaticProps: GetStaticProps = async (ctx) => {
       slugPost: slug,
     },
   });
-  console.log(data);
 
   return {
-    props: {},
+    props: {
+      post: data.post,
+    },
+    revalidate: 60 * 30, //30minutos
   };
 };
 
+//rotas dinamicas por causa GetStaticProps, dizendo a aplicação quais as rodas vão ser pré renderizadas na build.
 export const getStaticPaths: GetStaticPaths = async () => {
   return {
-    paths: [],
-    fallback: true,
+    paths: [{ params: { slug: "como-desenvolver-um-blog-com-nextjs" } }],
+    fallback: "blocking",
   };
 };
